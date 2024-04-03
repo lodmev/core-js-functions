5b22db45e4213a3543f55c215822e97169da7351
lodmev <lodmev@gmail.com>
2024-04-03 15:24:23 +0300

    feat: solve all tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -176,8 +176,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function executer(...args) {
+    const logOutput = `${func.name}(${args.map((arg) => JSON.stringify(arg))})`;
+    logFunc(`${logOutput} starts`);
+    const result = func(...args);
+    logFunc(`${logOutput} ends`);
+    return result;
+  };
 }
 
 /**
@@ -193,8 +199,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function partial(...args2) {
+    return fn.apply(this, [...args1, ...args2]);
+  };
 }
 
 /**
@@ -214,8 +222,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom - 1;
+  return function next() {
+    count += 1;
+    return count;
+  };
 }
 
 module.exports = {
